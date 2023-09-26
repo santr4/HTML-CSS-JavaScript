64c59debfb472f65616562546f03783bc3e74b0a
santr4 <santra.aaranyak16092003@gmail.com>
2023-09-26 20:32:22 +0530

    primitive datatypes

---
+++ JavaScript/tut4.js
@@ -0,0 +1,24 @@
+// ** primitives and objects
+
+// NNSSBBU - types of primitive datatypes.
+
+let a = null;
+let b = 54;
+let c = "santra";
+let d = Symbol("I am a Symbol");
+let e = true;
+let f = BigInt("235");
+let g = undefined;
+
+console.log(a,b,c,d,e,f,g);
+
+// block of code.
+{
+    let h = BigInt("47");
+    let i = BigInt("3");
+    console.log(h+i);
+}
+
+let h = 76;
+
+console.log(h);
