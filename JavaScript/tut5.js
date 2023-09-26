fd4319c1dc415d078ab6d59f0f6965d086e6e814
santr4 <santra.aaranyak16092003@gmail.com>
2023-09-26 20:49:39 +0530

    practice set

---
+++ JavaScript/tut5.js
@@ -0,0 +1,26 @@
+// Practice Set - 1
+
+// ** Q1
+
+let a = "santra";
+let b = 7;
+
+console.log(typeof(a));
+console.log(typeof(b));
+
+console.log(a+b);
+
+// ** Q2
+
+console.log(typeof(a+b));
+
+// ** Q3 - (we cant change the const object later.)
+
+const c = {
+    name : "santra",
+    age : 20
+}
+
+// c = "aaranyak";  --> this will throw an error.
+
+console.log(c) 
