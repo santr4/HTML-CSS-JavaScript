19133deeca2b9dd18473615811d5d4b48dfea67b
santr4 <santra.aaranyak16092003@gmail.com>
2023-09-26 21:14:21 +0530

    arithmatic operators

---
+++ JavaScript/tut6.js
@@ -0,0 +1,27 @@
+// ** operators and expressions
+
+// all the below 3 lines are valid programs
+// 77;
+// "harray bhai";
+// true;
+
+// ** arithmatic operators
+
+let a = 5;
+var b = 6;
+
+console.log("a+b = ",a+b);
+console.log("a-b = ",a-b);
+console.log("a*b = ",a*b);
+console.log("a/b = ",a/b);
+console.log("a**b = ",a**b);
+console.log("a%b = ",a%b);
+
+let c = 7;
+
+console.log(c--);
+console.log(c);
+
+console.log(c++);
+console.log(c);
+
