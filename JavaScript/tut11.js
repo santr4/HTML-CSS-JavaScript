693516e500a813fa0b3a180c5a0c6629cd52dea8
santr4 <santra.aaranyak16092003@gmail.com>
2023-09-28 20:27:11 +0530

    while loops and do while loops

---
+++ JavaScript/tut11.js
@@ -0,0 +1,18 @@
+// while loops
+
+let n = 10;
+console.log("enter natural numbers till 10");
+
+let i = 1;
+while (i <= n) {
+    console.log(i);
+    i++;
+}
+
+// do while loop
+
+let num = 10
+
+do {
+    console.log("Jai Shree Ram");
+} while (num < 2);
