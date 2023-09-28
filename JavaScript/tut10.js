0ac3c933021ac3ee0c9a9ec52f109ed09335c028
santr4 <santra.aaranyak16092003@gmail.com>
2023-09-28 20:01:00 +0530

    for loops

---
+++ JavaScript/tut10.js
@@ -0,0 +1,34 @@
+/*
+// for loops
+
+// we have to find the sum of first 10 natural numbers
+
+let sum = 0;
+
+for(let i = 1;i <= 10;i++){
+    sum += i;
+}
+
+console.log(sum);
+
+// for in loops
+
+let obj = {
+    'harry' : 0,
+    'santra' : 100,
+    'gaurav' : 99,
+    'himanshu' : 98,
+    'ansh' : 120
+}
+
+for(let b in obj){
+    console.log(b);
+}
+
+for(let c in obj){
+    console.log(obj[c]);
+}
+*/
+
+// for of loop
+
