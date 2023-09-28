c55dbd5471d291521f04c1e82ec890ebd8323bc4
santr4 <santra.aaranyak16092003@gmail.com>
2023-09-28 14:37:34 +0530

    switch case and ternary operator

---
+++ JavaScript/tut8.js
@@ -0,0 +1,18 @@
+// ** switch cases 
+
+let a = "santra";
+
+switch(a){
+    case 'rohan':
+        console.log("I am a musician");
+        break;
+    case 'santra':
+        console.log("I am a programmer");
+    case 'aaranyak':
+        console.log('I am a footballer');
+    default:
+        console.log('I love to travel');
+}
+
+let age = 6;
+console.log('I can', age<18?'not drive' : 'drive'); // ternary operator.
