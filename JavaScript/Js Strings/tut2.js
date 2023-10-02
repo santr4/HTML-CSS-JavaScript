0ad125826cf3226bb5d88d20f931b93e676ac66f
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-02 21:13:49 +0530

    strings methods

---
+++ JavaScript/Js Strings/tut2.js
@@ -0,0 +1,42 @@
+// ** String methods
+
+let text = "my name is aaranyak santra";
+console.log(text.slice(3,7));
+
+console.log(text.slice(3));
+
+console.log(text.slice(-15));
+
+let x = "cricket, football, chess";
+console.log("my favourite sport is: " + x.substring(9,17));
+
+console.log(x.substr(0,7));
+
+let y = "the best footballer in the world is messi";
+let z = y.replace("messi","ronaldo");
+
+console.log(z);
+
+// the replace function is case sensitive, to remove that use /i
+
+let b = y.replace("MESSI","aaranyak santra");
+let a = y.replace(/MESSI/i,"neymar");
+
+console.log(b);
+console.log(a);
+
+// use regular experssion /g to replace all the same words in the sentence
+
+let str = "Yash Katyan is intelligent. Everyone know that he is an intelligent boy";
+let str1 = str.replace(/intelligent/g,"bakchod");
+let str2 = str.replaceAll("intelligent","v-guard");
+
+console.log(str1);
+console.log(str2);
+
+let c = "Aaranyak Santra";
+let d = c.toUpperCase(c);
+console.log(d);
+
+// concat function
+
