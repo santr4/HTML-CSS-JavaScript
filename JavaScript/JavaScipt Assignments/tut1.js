0a284126df88008bcf05db8bf9039fe305f3dc45
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-02 12:45:21 +0530

    assignment operators

--- JavaScript/JavaScipt Assignments/tut1.js
+++ JavaScript/JavaScipt Assignments/tut1.js
@@ -8,7 +8,7 @@ console.log(x);
 
 let y = "aaa";
 let z = "a";
-console.log(y-z);  // NaN -> not a number
+console.log(y - z);  // NaN -> not a number
 
 // shift operator.
 let w = 10;
@@ -34,17 +34,17 @@ b ??= 100;
 
 c ??= 100;
 
-if(b == 100){
+if (b == 100) {
     console.log("b was previously null");
 }
 
-else{
+else {
     console.log("b was not previously null");
 }
 
-if(c == 100){
+if (c == 100) {
     console.log("c was previously null");
 }
-else{
+else {
     console.log("c was not previously null");
 }
