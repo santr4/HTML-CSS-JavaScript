da008abf66f96c9cb57b553d5f70a347e59f096e
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-02 12:33:17 +0530

    js arithmatic

---
+++ JavaScript/Js Operators/tut1.js
@@ -0,0 +1,18 @@
+// Js arithmatic
+
+
+let x = 5; let y = 6;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x / y);
+console.log(x * y);
+console.log(x ** y);
+console.log(x % y);
+
+console.log(--x);
+console.log(++y);
+
+
+
+console.log(5 * 6 / 2 - 8 + 7)
