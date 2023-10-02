fbb1958c353a4fd235db2aa4483dd06e39f40372
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-02 12:18:12 +0530

    js variables

---
+++ JavaScript/Js Variables/tut1.js
@@ -0,0 +1,17 @@
+// ** There are mainly three types of variables in js:
+
+/*     1. var
+       2. const
+       3. let
+*/
+
+var x = 1;
+let y = 6;
+const z = 9;
+
+y = 7;
+var x = 8;
+
+// const and let can't be re-declared only var can be re-declared.
+
+console.log(x + y + z);
