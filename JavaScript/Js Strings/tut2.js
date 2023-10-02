e77da50337125cf89ef7820705b9c105a2e551f0
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-02 21:21:42 +0530

    strings methods

--- JavaScript/Js Strings/tut2.js
+++ JavaScript/Js Strings/tut2.js
@@ -1,26 +1,26 @@
 // ** String methods
 
 let text = "my name is aaranyak santra";
-console.log(text.slice(3,7));
+console.log(text.slice(3, 7));
 
 console.log(text.slice(3));
 
 console.log(text.slice(-15));
 
 let x = "cricket, football, chess";
-console.log("my favourite sport is: " + x.substring(9,17));
+console.log("my favourite sport is: " + x.substring(9, 17));
 
-console.log(x.substr(0,7));
+console.log(x.substr(0, 7));
 
 let y = "the best footballer in the world is messi";
-let z = y.replace("messi","ronaldo");
+let z = y.replace("messi", "ronaldo");
 
 console.log(z);
 
 // the replace function is case sensitive, to remove that use /i
 
-let b = y.replace("MESSI","aaranyak santra");
-let a = y.replace(/MESSI/i,"neymar");
+let b = y.replace("MESSI", "aaranyak santra");
+let a = y.replace(/MESSI/i, "neymar");
 
 console.log(b);
 console.log(a);
@@ -28,8 +28,8 @@ console.log(a);
 // use regular experssion /g to replace all the same words in the sentence
 
 let str = "Yash Katyan is intelligent. Everyone know that he is an intelligent boy";
-let str1 = str.replace(/intelligent/g,"bakchod");
-let str2 = str.replaceAll("intelligent","v-guard");
+let str1 = str.replace(/intelligent/g, "bakchod");
+let str2 = str.replaceAll("intelligent", "v-guard");
 
 console.log(str1);
 console.log(str2);
@@ -51,9 +51,14 @@ console.log(h.trim());
 let i = "amrita vishwa vidyapeetham";
 let char = i.charAt(1);
 let char1 = i[1];
-if(char ==  char1){
+if (char == char1) {
     console.log('true');
 }
-else{
+else {
     console.log('false');
 }
+
+let str3 = "lets play football, cricket, and chess.";
+let str4 = str3.split(",");
+console.log(str4);
+console.log(typeof (str4));
