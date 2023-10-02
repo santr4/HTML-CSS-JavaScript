338608699801fd8c90b91e8bfa6e817ba271b037
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-02 12:14:07 +0530

    Js Syntax

---
+++ JavaScript/Js Syntax/prac1.js
@@ -0,0 +1,18 @@
+let x = 6;
+var y = 7;
+
+let z = x + y;
+
+console.log(z);
+
+// js is case sensitive
+
+let Variable, variable;
+
+Variable = 7;
+variable = "santra";
+
+console.log(Variable + variable);
+
+let w = "Aaranyak" + " " + "Santra";
+console.log(w);
