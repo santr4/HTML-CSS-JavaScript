1b4613d64cb4fd0a588cce55eda810fc7eeda8b0
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-05 19:38:22 +0530

    Js Switch

---
+++ JS Switch/tut1.js
@@ -0,0 +1,28 @@
+// (Sunday=0, Monday=1, Tuesday=2 ..)
+
+let day = "hello";
+
+switch (new Date().getDay()) {
+    case 0:
+        day = "sunday";
+        break;
+    case 1:
+        day = "monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+}
+
+console.log(day);
