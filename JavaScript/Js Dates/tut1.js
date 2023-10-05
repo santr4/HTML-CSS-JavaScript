fc8835e2bfcdbcc4b0823759b5e9dde62579c17b
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-05 19:35:12 +0530

    Js Dates

---
+++ JavaScript/Js Dates/tut1.js
@@ -0,0 +1,21 @@
+// JS Dates
+
+const date = new Date();
+console.log(date);
+
+const d = new Date("2023-10-5");
+console.log(d);
+
+// JS Date Get Methods
+
+// getFUllYear() Method
+
+const d1 = new Date("2023-10-05");
+console.log(d1.getFullYear());
+console.log(d1.getMonth());
+console.log(d1.getDate());
+console.log(d1.getHours());
+console.log(d1.getMinutes());
+console.log(d1.getSeconds());
+console.log(d1.getMilliseconds());
+console.log(d1.getDay());
